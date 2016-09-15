f8374758e1924e6f5af05617ca30d5867fd4ecc8
Evgeniy Demidov <eydemidov@gmail.com>
2016-09-15 08:46:55 +0300

    specs

--- spec/naive_similarity_spec.rb
+++ spec/naive_similarity_spec.rb
@@ -5,7 +5,13 @@ describe NaiveSimilarity do
     expect(NaiveSimilarity::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  describe ".similar" do
+    it "returns similar objects" do
+      arr = Array.new(10) { |i| {value1: i, value2: i ** 2}}
+
+      similar = NaiveSimilarity::similar(arr[3], arr, :value1, :value2)
+
+      expect(similar).to eq([{:value1=>3, :value2=>9}, {:value1=>4, :value2=>16}, {:value1=>5, :value2=>25}, {:value1=>2, :value2=>4}, {:value1=>6, :value2=>36}, {:value1=>7, :value2=>49}, {:value1=>8, :value2=>64}, {:value1=>9, :value2=>81}, {:value1=>1, :value2=>1}])
+    end
   end
 end
